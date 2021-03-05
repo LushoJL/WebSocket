b742e694832064052f6e0b2727a0e994d7a2f1d8
lushoJL <josespinaluque@gmail.com>
2021-03-05 15:12:50 -0400

    obtener dominio

--- socket.js
+++ socket.js
@@ -1,8 +1,18 @@
 module.exports = (io,url)=>{
     io.of(url).on('connection',socket=>{
-
         socket.on('send:pendiente',data=>{
-            io.of(url).emit('send:pendienteTabla',data);
+
+            switch (parseInt(data.CodDPM)){
+                case 1:
+                    io.of(url).emit('send:pendienteDelivery'+data.IdSucursal,data);
+                    break;
+                case 2:
+                    io.of(url).emit('send:pendientePickup'+data.IdSucursal,data);
+                    break;
+                case 3:
+                    io.of(url).emit('send:pendienteMesa'+data.IdSucursal,data);
+                    break;
+            }
         })
 
     })
